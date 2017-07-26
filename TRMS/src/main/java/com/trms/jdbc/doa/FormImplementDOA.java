cbf967974d7a544b2385627f3cdc160647672e08
WilliamRLewis <WilliamRWLewis@gmail.com>
2017-07-26 19:51:23 -0400

    Added a connection factory

--- TRMS/src/main/java/com/trms/jdbc/doa/FormImplementDOA.java
+++ TRMS/src/main/java/com/trms/jdbc/doa/FormImplementDOA.java
@@ -1,5 +1,22 @@
 package com.trms.jdbc.doa;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.trms.jdbc.util.ConnectionFactory;
+
 public class FormImplementDOA implements FormDOA {
+	private static Logger log = Logger.getRootLogger();
+
+	Connection conn = null;
+	public FormImplementDOA(){
+		setup();
+	}
+	
+	public void setup(){
+		//log.debug("Attempting to use Connection Factory");
+		conn = ConnectionFactory.getInstance().getConnection();
+		
+	}
 
 }
--- TRMS/src/main/java/com/trms/jdbc/util/ConnectionFactory.java
+++ TRMS/src/main/java/com/trms/jdbc/util/ConnectionFactory.java
@@ -1,6 +1,63 @@
 package com.trms.jdbc.util;
 
-public class ConnectionFactory {
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
-	
-}
+import org.apache.log4j.Logger;
+
+public class ConnectionFactory{
+		private static ConnectionFactory cf = null;
+		private static Boolean build = true;
+		private static Logger log = Logger.getRootLogger();
+		private ConnectionFactory(){
+			
+		}
+		
+		public static synchronized ConnectionFactory getInstance(){
+			if(build){
+				cf = new ConnectionFactory();
+			}
+			return cf;
+		}
+		
+		public Connection getConnection(){
+			log.debug("Starting connection");
+			Connection conn = null;
+			Properties prop = new Properties();
+
+			try{
+				
+			FileWriter fw = new FileWriter("test.txt");
+			fw.write("hello");
+			fw.flush();
+			
+			prop.load(new FileReader("./datasource.properties"));
+																					
+		} catch(IOException e1){
+			e1.printStackTrace();
+			log.debug("Failed to read Props file");}
+		
+		try{
+			conn = DriverManager.getConnection(
+					prop.getProperty("url"),
+					prop.getProperty("username"), 
+					prop.getProperty("password"));
+		//	log.debug("Read URL and login");
+		} catch(SQLException e){
+			log.debug("Faild to read url username or password for database");
+			e.printStackTrace();
+		}
+		return conn;
+		}
+		
+		
+		
+	}
+
+
+
