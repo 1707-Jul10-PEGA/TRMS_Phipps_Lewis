e0edb2dc21c5740a3440411c52c4c15ced049d52
WilliamRLewis <WilliamRWLewis@gmail.com>
2017-07-27 18:15:03 -0400

    removed jars from git ignore

--- TRMS/src/test/java/com/trms/jdbc/doa/FormDOATest.java
+++ TRMS/src/test/java/com/trms/jdbc/doa/FormDOATest.java
@@ -15,6 +15,7 @@ import com.trms.jdbc.util.ConnectionFactory;
 public class FormDOATest {
 	Logger logTest = Logger.getRootLogger();
 	static Connection conn = null;
+	static FormImplementDOA formDOA = new FormImplementDOA();
 	
 	@BeforeClass
 	public static void connSetup(){
@@ -32,15 +33,20 @@ public class FormDOATest {
 	//Test the form cost set methods (2 methods)
 	@Test
 	public void testGetCostOnFormIDMethods() throws SQLException{
-		
+	
 
 		
 	}
 	
-	
+	@Test
+	public void testSetCostOnFormIDMethods() throws SQLException{
 	//Test the form cost get methods (2 methods)
-	assertTrue("Ensure output is 0 on negative input", setFullCostOnFormID(1, 200));
+	assertTrue("Ensure output is 0 on negative input", formDOA.setFullCostOnFormID(1, -200.00) == 0.0);
+	assertTrue("Ensure output is 0 on 0 input", formDOA.setFullCostOnFormID(1, 00.00) == 0.0);
 	
+	
+	
+	}
 //	public double getReimbursementCostOnFormID(Integer formID)throws SQLException;
 //	public double getFullCostOnFormID(Integer formID)throws SQLException;
 //	public double setFullCostOnFormID(Integer formID, Double newCost) throws SQLException;
