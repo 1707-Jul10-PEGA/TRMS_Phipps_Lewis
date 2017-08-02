215e91ba0f50b7b05cd127653e332b5247530f8b
wphipps <wphipps@wau.edu>
2017-08-01 20:07:38 -0400

    updates on viewapps

--- TRMS/src/main/webapp/resources/js/viewapps.js
+++ TRMS/src/main/webapp/resources/js/viewapps.js
@@ -1,34 +1,42 @@
 $(document).ready(function() {
 	
 	$.post("GetFormServlet", function (data) {
-		var tr;
-		for(var i = 0; i < data.length; i++){
-			tr = $('<tr/>');
-			tr.append("<td>" + data[i].firstname + "</td>");
-			tr.append("<td>" + data[i].lastname + "</td>");
-			tr.append("<td>" + data[i].date_made + "</td>");
-			tr.append("<td>" + data[i].status + "</td>");
-			tr.append("<td>" + data[i].grade_score + "</td>");
-			$('myappstable').append(tr);
-			
-		 	
-		}
+		
 		
 		var splitter = data.split(";");
-		splitter
+		
 		var tr;
-		tr = $('<tr/>');
+		tr = $("<tr/>")
 		for(var i = 0; i < splitter.length; i++){
-			tr.append("<td>" + splitter[i + 1]+ "</td>");
+			if(i % 5 == 0){
+				tr.append("<tr/>")
+			}
+			tr.append("<td>" + splitter[i]+ "</td>");
 		}
 		
 		$('#myappstable').append(tr);
-		window.alert(data);
-
-		window.alert(data.split(";")[1]);
+		
+		
+	
 	});
 	
+	$.post("AllFormsServlet", function (data) {
+		
+		
+		var splitter = data.split(";");
+
+		var tr;
+		tr = $('<tr/>');
+		for(var x = 0; x < (spliiter.length / 5); x++) {
+			//tr.append("<tr/>");
+			for(var i = 0; i < splitter.length; i++){
+				tr.append("<td>" + splitter[i] + "</td>");
+			}
+			tr.append("<tr/>");
+		}
+		$('#allappstable').append(tr);
 	
+	});
 	
 	
 });
